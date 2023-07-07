91280c4a926ee621d876ce91a2de95f9436e1d90
Marcos Artimundo <51058488+martimundo@users.noreply.github.com>
2023-07-07 14:35:22 -0300

    Inicio do projeto

---
+++ client-server/public/classes/HttpRequest.js
@@ -0,0 +1,57 @@
+class HttpRequest {
+
+    static get(url, params = {}) {
+
+        return HttpRequest.request('GET', url, params);
+    }
+    static delete(url, params = {}) {
+
+        return HttpRequest.request('DELETE', url, params);
+    }
+    static put(url, params = {}) {
+
+        return HttpRequest.request('PUT', url, params);
+    }
+    static post(url, params = {}) {
+
+        return HttpRequest.request('POST', url, params);
+    }
+
+    static request(method, url, params = {}) {
+
+        return new Promise((resolve, reject) => {
+
+            let ajax = new XMLHttpRequest();
+
+            let obj = {};
+
+            ajax.open(method.toUpperCase(), url);
+
+            ajax.onerror = event => {
+                reject(e);
+            }
+
+            ajax.onload = event => {
+
+                try {
+
+                    obj = JSON.parse(ajax.responseText);
+
+                } catch (e) {
+                    reject(e);
+                    //console.error(e);
+                }
+
+                resolve(obj);
+
+            }
+            
+            ajax.setRequestHeader('Content-Type', 'application/json');
+
+            ajax.send(JSON.stringify(params));
+
+        });
+
+
+    }
+}
---
+++ client-server/routes/users.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var assert = require('assert');
+var restify = require('restify-clients');
+var router = express.Router();
+
+var client = restify.createJsonClient({
+  url: 'http://localhost:4000'
+});
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+
+  client.get('/users', function (err, request, response, obj) {
+
+    assert.ifError(err);
+
+    res.end(JSON.stringify(obj, null, 2));
+
+    //console.log(JSON.stringify(obj, null, 2));
+
+  });
+
+});
+
+router.get('/:id', function (req, res, next) {
+
+  client.get(`/users/${req.params.id}`, function (err, request, response, obj) {
+
+    assert.ifError(err);
+
+    res.json(obj);
+
+    //console.log(JSON.stringify(obj, null, 2));
+
+  });
+
+});
+
+/**
+ * rota para update.
+ * O req.body serve para enviar os dados que estam sendo atualizados.
+ */
+router.put('/:id', function (req, res, next) {
+
+  client.get(`/users/${req.params.id}`, req.bady, function (err, request, response, obj) {
+
+    assert.ifError(err);
+
+    res.json(obj);
+
+    //console.log(JSON.stringify(obj, null, 2));
+
+  });
+
+});
+/**
+ * Esta rota fara a exclusão de um item. 
+ * O restify utiliza o del ao inves do delete.
+ */
+router.delete('/:id', function (req, res, next) {
+
+  client.del(`/users/${req.params.id}`, function (err, request, response, obj) {
+
+    assert.ifError(err);
+
+    res.json(obj);
+
+    //console.log(JSON.stringify(obj, null, 2));
+
+  });
+
+});
+/**
+ * Rota para salvar o primeiro registro no banco de dados.
+ */
+
+router.post('/', function (req, res, next) {
+
+  client.post('/users/', req.bady, function (err, request, response, obj) {
+
+    assert.ifError(err);
+
+    res.json(obj);
+
+    //console.log(JSON.stringify(obj, null, 2));
+
+  });
+
+});
+
+module.exports = router;
